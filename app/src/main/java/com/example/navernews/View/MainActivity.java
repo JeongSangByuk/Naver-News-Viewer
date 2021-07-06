c8e80c438c296353059c7063dcbee96750a1db0f
sangbyuk <jsb100800@naver.com>
2021-07-06 17:03:01 +0900

    [Update] main acitivity layout title

--- app/src/main/java/com/example/navernews/View/MainActivity.java
+++ app/src/main/java/com/example/navernews/View/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     private void initObject(){
         tv1 = findViewById(R.id.tv1);
         tv2 = findViewById(R.id.tv2);
-        tv3 = findViewById(R.id.tv3);
+        tv3 = findViewById(R.id.tv33);
         bnt1 = findViewById(R.id.bnt1);
     }
 
